eb801194c00f86bd86db28b0dd87e73de88e0db5
Gregor Wolf <gregor.wolf@gmail.com>
2023-02-05 23:32:18 +0100

    chore: restructure

--- srv/customer-material-service.cds
+++ srv/customer-material-service.cds
@@ -1,18 +1,18 @@
 //using { API_PRODUCT_SRV as externalProduct } from './external/API_PRODUCT_SRV.csn';
 //using { API_BUSINESS_PARTNER as externalBusinessPartner } from './external/API_BUSINESS_PARTNER.csn';
-using { API_CUSTOMER_MATERIAL_SRV as externalCustomerMaterial } from './external/API_CUSTOMER_MATERIAL_SRV.csn';
+using {API_CUSTOMER_MATERIAL_SRV as externalCustomerMaterial} from './external/API_CUSTOMER_MATERIAL_SRV';
 
 service CustomerMaterialService {
 
-  entity A_CustomerMaterial as projection on externalCustomerMaterial.A_CustomerMaterial;
-  entity A_CustomerMaterialSimple as projection on externalCustomerMaterial.A_CustomerMaterial;
+  entity A_CustomerMaterial        as projection on externalCustomerMaterial.A_CustomerMaterial;
+  entity A_CustomerMaterialComplex as projection on externalCustomerMaterial.A_CustomerMaterial;
 
 }
 
-annotate CustomerMaterialService with @(requires: 'authenticated-user');
-// Feature not supported: Method .0 of fluent API 
+annotate CustomerMaterialService with @(requires : 'authenticated-user');
+// Feature not supported: Method .0 of fluent API
 /*
-annotate CustomerMaterialService.A_CustomerMaterial with @(restrict: [ 
-  { grant: 'READ', where: 'Customer = $user.customer' } 
+annotate CustomerMaterialService.A_CustomerMaterial with @(restrict: [
+  { grant: 'READ', where: 'Customer = $user.customer' }
 ]);
 */
--- srv/customer-material-service.js
+++ srv/customer-material-service.js
@@ -1,33 +1,58 @@
-const cds = require('@sap/cds')
+const cds = require("@sap/cds");
 
 function getCustomerFilter(req) {
-  var customer = req.user.attr.customer
-  return {Customer: customer}
+  var customer = req.user.attr.customer;
+  return { Customer: customer };
 }
 
-module.exports = async function (srv){
+module.exports = async function (srv) {
+  const externalCustomerMaterial = await cds.connect.to(
+    "API_CUSTOMER_MATERIAL_SRV"
+  );
 
-  const externalCustomerMaterial = await cds.connect.to('API_CUSTOMER_MATERIAL_SRV')
-
-  srv.on ('READ','A_CustomerMaterial', async req => {
-    const externalCustomerMaterialTransaction = externalCustomerMaterial.transaction(req)
+  srv.on("READ", "A_CustomerMaterial", async (req) => {
+    const externalCustomerMaterialTransaction =
+      externalCustomerMaterial.transaction(req);
     try {
       // Restrict to Customer in User attribute
-      var customerFilter = getCustomerFilter(req)
+      var customerFilter = getCustomerFilter(req);
+      req.query.where(customerFilter);
+      let result = await externalCustomerMaterialTransaction.run(req.query);
+      return result;
+    } catch (error) {
+      console.error("Error Message: " + error.message);
+      if (error.request && error.request.path) {
+        console.error("Request Patch: " + error.request.path);
+      }
+    }
+  });
+
+  srv.on("READ", "A_CustomerMaterialComplex", async (req) => {
+    const externalCustomerMaterialTransaction =
+      externalCustomerMaterial.transaction(req);
+    try {
+      // Restrict to Customer in User attribute
+      var customerFilter = getCustomerFilter(req);
       // Works but needs custom handling for read
-      if(req.query.SELECT.from && req.query.SELECT.from.ref[0] && req.query.SELECT.from.ref[0].where) {
+      if (
+        req.query.SELECT.from &&
+        req.query.SELECT.from.ref[0] &&
+        req.query.SELECT.from.ref[0].where
+      ) {
         // single read
-        var query = SELECT.from(req.target).where(req.data).where(customerFilter)
-        if(req.query.SELECT.columns) {
+        var query = SELECT.from(req.target)
+          .where(req.data)
+          .where(customerFilter);
+        if (req.query.SELECT.columns) {
           query.columns();
         }
-        let results = await externalCustomerMaterialTransaction.run(query)
-        return results[0]
+        let results = await externalCustomerMaterialTransaction.run(query);
+        return results[0];
       } else {
         // Query
-        req.query.where(customerFilter)
-        let result = await externalCustomerMaterialTransaction.run(req.query)
-        return result
+        req.query.where(customerFilter);
+        let result = await externalCustomerMaterialTransaction.run(req.query);
+        return result;
       }
       /*
       // Creates:
@@ -38,33 +63,17 @@ module.exports = async function (srv){
       return result
       */
     } catch (error) {
-      console.error("Error Message: " + error.message)
-      if(error.request && error.request.path) {
-        console.error("Request Patch: " + error.request.path)
+      console.error("Error Message: " + error.message);
+      if (error.request && error.request.path) {
+        console.error("Request Patch: " + error.request.path);
       }
     }
-  })
-
-  srv.on ('READ','A_CustomerMaterialSimple', async req => {
-    const externalCustomerMaterialTransaction = externalCustomerMaterial.transaction(req)
-    try {
-      // Restrict to Customer in User attribute
-      var customerFilter = getCustomerFilter(req)
-      req.query.where(customerFilter)
-      let result = await externalCustomerMaterialTransaction.run(req.query)
-      return result
-    } catch (error) {
-      console.error("Error Message: " + error.message)
-      if(error.request && error.request.path) {
-        console.error("Request Patch: " + error.request.path)
-      }
+  });
+  /*
+  srv.after("READ", "A_CustomerMaterial", (customerMaterial, req) => {
+    if (customerMaterial && customerMaterial.length) {
+      customerMaterial.$count = customerMaterial.length;
     }
-  })
-
-  srv.after('READ', 'A_CustomerMaterial', (customerMaterial,req) => {
-    if(customerMaterial && customerMaterial.length) {
-      customerMaterial.$count = customerMaterial.length
-    }
-  })
-
-}
+  });
+  */
+};
