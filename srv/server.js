f87b793b4d1edace2364fbea885da53d98193b96
Gregor Wolf <gregor.wolf@gmail.com>
2023-02-13 21:25:47 +0100

    add cds-launchpad-plugin

--- srv/server.js
+++ srv/server.js
@@ -4,8 +4,22 @@ const port = process.env.PORT || 4004;
 const cds = require("@sap/cds");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 
-cds.on("bootstrap", app => app.use(proxy({
-  port: port
-})));
+if (process.env.NODE_ENV !== "production") {
+  const { cds_launchpad_plugin } = require("cds-launchpad-plugin");
+
+  // Enable launchpad plugin
+  cds.once("bootstrap", (app) => {
+    const handler = new cds_launchpad_plugin();
+    app.use(handler.setup({ theme: "sap_horizon", version: "1.108.5" }));
+  });
+}
+
+cds.on("bootstrap", (app) =>
+  app.use(
+    proxy({
+      port: port,
+    })
+  )
+);
 
 module.exports = cds.server;
